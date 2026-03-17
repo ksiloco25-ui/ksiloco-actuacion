78d49cfacdd72cf1741b7a129249e6e95af17390
ksiloco25-ui <ksiloco25@gmail.com>
2026-03-17 18:55:41 +0000

    v2

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/ksi-clown/', // GitHub Pages repo path
+    base: '/ksiloco-actuacion/', // GitHub Pages repo path
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
